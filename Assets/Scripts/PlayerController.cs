ec1c143cf3c9ef4087c6f70150e7db874c01c556
Yee-Kun Jung <48750313+Twoketchupplz@users.noreply.github.com>
2022-02-11 19:23:34 +0900

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,20 +4,45 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-	public float MinEP;
-	public float MaxEP;
-	VehicleController Vctrl;
-	// TankController Vctrl;
-	// Start is called before the first frame update
-	void Start()
-	{
-		Vctrl = new TankController(transform, MinEP, MaxEP);
-		Debug.Log("시작");
-		Debug.Log("파킹브레이크를 풀어주세요.");
-	}
-	// Update is called once per frame
-	void Update()
-	{
-		Vctrl.Move();
-	}
+    bool ToggleKeyO = true; // Truck or Tank
+	GameObject TankPrefab, TruckPrefab;
+
+    VehicleController Vctrl;
+    void Start()
+    {
+		TankPrefab = transform.GetChild(0).gameObject;
+		TruckPrefab = transform.GetChild(1).gameObject;
+		TankPrefab.SetActive(false);
+		TruckPrefab.SetActive(true);
+        Vctrl = new VehicleController(transform);
+        Debug.Log("시작");
+        Debug.Log("파킹브레이크를 풀어주세요.");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Vctrl.Move();
+		if (Input.GetKeyDown(KeyCode.O)) SwitchVehicle(ToggleKeyO);
+    }
+
+    // TODO: 플레이어가 Vehicle을 선택
+    void SwitchVehicle(bool toggle)
+    {
+        if (toggle) // change to Tank
+        {
+			Vctrl = new TankController(transform);
+			// 프리펩 전환
+			TruckPrefab.SetActive(false);
+			TankPrefab.SetActive(true);
+			ToggleKeyO  = false;
+        }
+        else // change to Truck
+        {
+            Vctrl = new VehicleController(transform);
+            // 프리펩 전환
+            TruckPrefab.SetActive(true);
+            TankPrefab.SetActive(false);
+			ToggleKeyO = true;
+        }
+    }
 }
